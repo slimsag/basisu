1c672a08bea3f0d45e4acf0da9baf58a42cc648c
LordMZTE <lord@mzte.de>
2023-04-16 21:19:04 +0200

    Use Zig package manager

---
+++ build.zig
@@ -0,0 +1,63 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) !void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const build_encoder = b.option(bool, "encoder", "Build the basisu encoder") orelse true;
+    const build_transcoder = b.option(bool, "transcoder", "build the basisu transcoder") orelse true;
+
+    if (!build_encoder and !build_transcoder) {
+        std.log.err("I'm supposed to build... nothing?", .{});
+        return error.NothingToBuild;
+    }
+
+    const lib = b.addStaticLibrary(.{
+        .name = "basisu",
+        .target = target,
+        .optimize = optimize,
+    });
+
+    lib.linkLibCpp();
+
+    lib.defineCMacro("BASISU_FORCE_DEVEL_MESSAGES", "0");
+    lib.defineCMacro("BASISU_SUPPORT_KTX2_ZSTD", "0");
+
+    if (build_encoder) {
+        lib.addCSourceFiles(&encoder_sources, &.{});
+    }
+
+    if (build_transcoder) {
+        lib.addCSourceFiles(&transcoder_sources, &.{
+            "-Wno-deprecated-builtins",
+            "-Wno-deprecated-declarations",
+            "-Wno-array-bounds",
+        });
+    }
+
+    lib.install();
+}
+
+const encoder_sources = [_][]const u8{
+    "encoder/basisu_backend.cpp",
+    "encoder/basisu_basis_file.cpp",
+    "encoder/basisu_bc7enc.cpp",
+    "encoder/basisu_comp.cpp",
+    "encoder/basisu_enc.cpp",
+    "encoder/basisu_etc.cpp",
+    "encoder/basisu_frontend.cpp",
+    "encoder/basisu_gpu_texture.cpp",
+    "encoder/basisu_kernels_sse.cpp",
+    "encoder/basisu_opencl.cpp",
+    "encoder/basisu_pvrtc1_4.cpp",
+    "encoder/basisu_resample_filters.cpp",
+    "encoder/basisu_resampler.cpp",
+    "encoder/basisu_ssim.cpp",
+    "encoder/basisu_uastc_enc.cpp",
+    "encoder/jpgd.cpp",
+    "encoder/pvpngreader.cpp",
+};
+
+const transcoder_sources = [_][]const u8{
+    "transcoder/basisu_transcoder.cpp",
+};
